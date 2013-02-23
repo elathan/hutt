88eef047d3e69294a92f470175f5e3f25d00960e
Elias Athanasopoulos <elathan@akasaka.local>
2013-02-23 18:09:32 -0500

    One more comment.

--- hutt.rb
+++ hutt.rb
@@ -2,6 +2,10 @@
 # A simple script for fetching Hacker News and store them in a Unix mailbox file.
 # You can read the news later using mutt (or other e-mail client).
 #
+# I have done the fetching part only and of course many things are missing.
+# Adding posting features is interesting; I guess you need to setup a custom
+# local MTA to do the job.
+#
 # The API for fetching the news is taken from: http://hndroidapi.appspot.com
 #
 # The code is free. 
